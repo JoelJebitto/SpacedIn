06ad5ed8ed0d6514d5ab0695e429f532bfbb740c
Joel Jebitto <joeljebitto@gmail.com>
2025-06-22 18:03:20 +0530

    created a card component

---
+++ SpacedIn/src/components/Deck.jsx
@@ -0,0 +1,15 @@
+import LearnButton from "./LearnButton";
+
+export default function Deck({ title, description, id }) {
+  return (
+    <div
+      className="bg-black/50 flex flex-col mx-auto my-2 p-5 text-white w-full lg:w-[47%] rounded-xl"
+      key={id}
+    >
+      <h1 className="text-3xl font-bold">{title}</h1>
+      <p className="text-gray-100">{description}</p>
+
+      <LearnButton className="mt-20" />
+    </div>
+  );
+}
--- SpacedIn/src/components/Decks.jsx
+++ SpacedIn/src/components/Decks.jsx
@@ -1,8 +1,37 @@
+import Deck from "./Deck";
+
+const lDeck = [
+  {
+    title: "English",
+    description: "Consepts of Class 11",
+    id: 0,
+  },
+  {
+    title: "Math",
+    description: "Consepts of Class 11",
+    id: 1,
+  },
+  {
+    title: "Science",
+    description: "Consepts of Class 11",
+    id: 2,
+  },
+  {
+    title: "Chemistry",
+    description: "Consepts of Class 11",
+    id: 3,
+  },
+];
+
 const Decks = () => {
   return (
-    <div className="mt-5">
-      <h1 className="text-3xl font-bold">Decks</h1>
-      <div className="p-5 bg-gray-400"></div>
+    <div className="mt-7 p-5 bg-black/80 text-white rounded-xl">
+      {/* <h1 className="text-4xl font-bold mb-3 underline">Decks:</h1> */}
+      <div className=" flex flex-wrap">
+        {lDeck.map((i) => (
+          <Deck {...i} />
+        ))}
+      </div>
     </div>
   );
 };
