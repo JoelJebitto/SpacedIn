c7bac4fccb609105401211b56cad806b55ae034e
Joel Jebitto <joeljebitto@gmail.com>
2025-06-29 10:16:15 +0530

    Add deck and card management frontend

--- SpacedIn/src/components/Cards.jsx
+++ SpacedIn/src/components/Cards.jsx
@@ -1,14 +1,11 @@
 import Card from "./Card";
 
-const Cards = () => {
+const Cards = ({ cards = [] }) => {
   return (
     <div className="bg-black/80 p-5 flex flex-wrap rounded-xl shadow-2xl mt-10">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {cards.map((c) => (
+        <Card key={c.id} {...c} />
+      ))}
     </div>
   );
 };
--- SpacedIn/src/components/Deck.jsx
+++ SpacedIn/src/components/Deck.jsx
@@ -1,4 +1,5 @@
 import LearnButton from "./LearnButton";
+import { Link } from "react-router-dom";
 
 export default function Deck({ title, description, id, createdOn }) {
   const dateObject = new Date(createdOn);
@@ -11,16 +12,16 @@ export default function Deck({ title, description, id, createdOn }) {
   };
   const formattedDate = dateObject.toLocaleDateString(undefined, options);
   return (
-    <div
+    <Link
+      to={`/deck/${id}`}
       className="bg-black/50 flex flex-col mx-auto my-2 p-5 text-white w-full md:w-[47%] rounded-xl shadow-2xl"
-      key={id}
     >
       <h1 className="text-3xl font-bold">{title}</h1>
       <p className="text-gray-300 text-md">{formattedDate}</p>
-      <hr class="border-2 border-gray-800 my-2 " />
+      <hr className="border-2 border-gray-800 my-2 " />
       <p className="text-gray-100 flex-1 text-lg">{description}</p>
 
       <LearnButton className="mt-20" />
-    </div>
+    </Link>
   );
 }
